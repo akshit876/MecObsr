e98e3983ceba1fc631ae4099dbe2c558caaaaebc
akshit <s.akshit1267@gmail.com>
2024-11-14 08:41:43 +0530

    correct navigation

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -21,7 +21,7 @@ const Login = () => {
     if (session?.user) {
       const role = session.user.role;
       if (role === 'operator') {
-        router.push('/part-number-select');
+        router.push('/');
       } else {
         router.push('/');
       }
