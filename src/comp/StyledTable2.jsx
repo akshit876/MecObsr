8d09745be1af3ebe4e10f923e06b071d7f338f3e
akshit <s.akshit1267@gmail.com>
2024-12-12 17:58:17 +0530

    Update StyledTable2 component to correctly display scanner data by using the updated value retrieval method. This change ensures that if the result is 'NG', the full value is shown; otherwise, the last character is removed, enhancing data presentation and user understanding of scanner results.

--- src/comp/StyledTable2.jsx
+++ src/comp/StyledTable2.jsx
@@ -29,7 +29,7 @@ const columns = [
     header: 'Scanner Data',
     cell: (info) => (
       <div className="font-medium text-gray-600">
-        {info.row.original.Result === 'NG' 
+        {info.getValue() === 'NG' 
           ? info.getValue() 
           : info.getValue()?.slice(0, -1)}
       </div>
